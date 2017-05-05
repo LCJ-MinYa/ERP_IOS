9c0d051d3a6f90fcd29039120ad12dc3d0d22a8c
LCJ-MinYa <1049468118@qq.com>
2017-05-05 09:30:06 +0800

    Init

---
+++ ERP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ERP
+//
+//  Created by minya on 2017/5/5.
+//  Copyright © 2017年 minya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
