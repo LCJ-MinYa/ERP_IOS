e50742fbcc355d1777396eb189157ddbd35f027a
LCJ-MinYa <1049468118@qq.com>
2017-05-05 09:45:19 +0800

    定义入口

---
+++ ERP/Enter(程序入口)/AppDelegate.m
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.m
+//  ERP
+//
+//  Created by minya on 2017/5/5.
+//  Copyright © 2017年 minya. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    //程序启动入口
+    
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+}
+
+
+@end
